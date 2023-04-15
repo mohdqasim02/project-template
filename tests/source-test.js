27ac48b2394adf32302fd4ab4e824bf55d975bfe
Mohd Qasim <qkqasimkhan02@gmail.com>
2023-04-15 16:57:55 +0530

    added test cases

--- tests/source-test.js
+++ tests/source-test.js
@@ -1,28 +1,51 @@
 const testing = require("../lib/test-framework.js");
 const number = require("../src/source.js");
 
-const assertArrayEquals = testing.assertArrayEquals;
-const assertObjectEquals = testing.assertObjectEquals;
+const areArraysEqual = testing.areArraysEqual;
+const are2dArrayEquals = testing.are2dArrayEquals;
 const assertEquals = testing.assertEquals;
 const summary = testing.summary;
 const title = testing.title;
 
-const it = function (testName, funcName, testData) {
+let it = function (testName, funcName, testData) {
   assertEquals(testData.expected, testData.actual, testName, funcName);
 };
 
-const test = function() {
+const testAssertEquals = function() {
 
-  it("assertEquals should pass", "assertEquals", {
+  it("should pass if elements are equal", "assertEquals", {
     expected: number.zero, 
     actual: number.zero 
   });
 
-  it("assertEquals should fail", "assertEquals", {
-    expected: number.zero,
-    actual: number.one
+  it("should fail if elements are not equal", "assertEquals", {
+    expected: false,
+    actual: number.one === number.zero,
+  });
+}
+
+const testAssertArrayEquals = function() {
+
+  it("should pass when two list are equal", "assertEquals", {
+    expected: true, 
+    actual: areArraysEqual([1, 2, 3], [1, 2, 3])
   });
 
+  it("should fail when two list are not equal", "assertEquals", {
+    expected: false, 
+    actual: areArraysEqual([1, 2, 4], [1, 2, 3])
+  });
+
+  it("should fail when size two list are not equal", "assertEquals", {
+    expected: false, 
+    actual: areArraysEqual([1, 2], [1, 2, 3])
+  });
+}
+
+const test = function() {
+  title("Testing test-framework.js");
+  testAssertEquals();
+  testAssertArrayEquals();
   summary();
 }
 
